598497c3fea225e69675089a25b6a92a9a79fc29
william <liyijiang3000@163.com>
2023-05-03 03:53:02 -0400

    add CLIP_FinetuneWithTextFreeze

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -1,6 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .impl.original import *
-from .impl.clip import CLIP, CLIP_LP, CLIP_Finetune
+from .impl.clip import CLIP, CLIP_LP, CLIP_Finetune, CLIP_FinetuneWithTextFreeze
 from .impl.w2d import W2D, W2D_v2, W2D_EMA, W2D_v2_EMA
 from .impl.clip_kd import ERM_CLIP_Logits, W2D_v2_CLIP_Logits, W2D_v2_CLIP_Logits_EMA, ERM_CLIP_Logits_EMA, ERM_SMA_HardExampleMining, ERM_SMA_CLIPDistill
 from .impl.sma import ERM_SMA
@@ -48,7 +48,8 @@ ALGORITHMS = [
     'W2D_v2_CLIP_Logits_EMA',
     'ERM_CLIP_Logits_EMA',
     'ERM_SMA_HardExampleMining',
-    'ERM_SMA_CLIPDistill'
+    'ERM_SMA_CLIPDistill',
+    'CLIP_FinetuneWithTextFreeze'
 ]
 
 def get_algorithm_class(algorithm_name):
--- domainbed/impl/clip.py
+++ domainbed/impl/clip.py
@@ -1,5 +1,5 @@
 import torch
-
+from torch.nn import functional as F
 from domainbed import networks
 import clip
 from .base import Algorithm
@@ -81,3 +81,35 @@ class CLIP_Finetune(ERM):
   
     def predict(self, x):
         return self.classifier(self.featurizer.forward_image(x))
+
+class CLIP_FinetuneWithTextFreeze(ERM): 
+    def __init__(self, input_shape, num_classes, num_domains, hparams):
+        super(CLIP_FinetuneWithTextFreeze, self).__init__(input_shape, num_classes, num_domains, hparams)
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        self.featurizer = networks.CLIP(self.hparams)
+        self.featurizer.eval() # turn off bn update
+        
+        self.optimizer = torch.optim.Adam(
+            self.featurizer.clip_model.visual.parameters(),
+            lr=self.hparams["lr"],
+            weight_decay=self.hparams['weight_decay']
+        )
+        self.prompt = torch.cat([clip.tokenize(f'a photo of a {cls_name}') for cls_name in hparams['class_names']]).to(self.device)
+        
+    def update(self, minibatches, unlabeled=None):
+        self.train()
+        all_x = torch.cat([x for x, y in minibatches])
+        all_y = torch.cat([y for x, y in minibatches])
+        logits_per_image, _ = self.model(all_x, self.prompt)
+        loss = F.cross_entropy(logits_per_image, all_y)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return {'loss': loss.item()}
+
+    def predict(self, x):
+        logits_per_image, _ = self.model(x, self.prompt)
+        return logits_per_image.softmax(dim=-1)
