b7f06f8c6a5d938320e5b46ea47d9319041f3738
william <yli556@jhu.edu>
2023-05-05 17:39:07 -0500

    add tools

---
+++ tools/dist_trail_nohpsearch.sh
@@ -0,0 +1,30 @@
+dataset=$1
+algorithms=$2
+save_path=work_dirs/sweep/vit-b16/$dataset/${algorithms}
+
+python -m domainbed.scripts.sweep delete_incomplete\
+       --datasets ${dataset}\
+       --algorithms $algorithms \
+       --data_dir ./domainbed/data\
+       --command_launcher multi_gpu\
+       --single_test_envs\
+       --steps 5001\
+       --holdout_fraction 0.2\
+       --n_hparams 1\
+       --n_trials 3\
+       --skip_confirmation\
+       --hparams "$(<configs/clipood_vit-b16.json)"\
+       --output_dir $save_path
+CUDA_VISIBLE_DEVICES=4,5 python -m domainbed.scripts.sweep launch\
+       --datasets ${dataset}\
+       --algorithms $algorithms \
+       --data_dir ./domainbed/data\
+       --command_launcher multi_gpu\
+       --single_test_envs\
+       --steps 5001\
+       --holdout_fraction 0.2\
+       --n_hparams 1\
+       --n_trials 3\
+       --skip_confirmation\
+       --hparams "$(<configs/clipood_vit-b16.json)"\
+       --output_dir $save_path
