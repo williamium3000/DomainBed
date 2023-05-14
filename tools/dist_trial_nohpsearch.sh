172946ea837c34a2568d9b4656678d8b8bbb9af9
william <liyijiang3000@163.com>
2023-05-14 05:12:38 +0000

    fix bug

--- tools/dist_trial_nohpsearch.sh
+++ tools/dist_trial_nohpsearch.sh
@@ -1,6 +1,6 @@
 dataset=$1
 algorithms=$2
-save_path=work_dirs/sweep/vit-b16/${dataset}_extra/${algorithms}
+save_path=work_dirs/sweep/vit-b16_autoaug/${dataset}/${algorithms}
 
 python -m domainbed.scripts.sweep delete_incomplete\
        --datasets ${dataset}\
@@ -15,7 +15,7 @@ python -m domainbed.scripts.sweep delete_incomplete\
        --skip_confirmation\
        --hparams "$(<configs/clipood_vit-b16_with_extra.json)"\
        --output_dir $save_path
-CUDA_VISIBLE_DEVICES=3,4,5 python -m domainbed.scripts.sweep launch\
+CUDA_VISIBLE_DEVICES=4,5,6 python -m domainbed.scripts.sweep launch\
        --datasets ${dataset}\
        --algorithms $algorithms \
        --data_dir ./domainbed/data\
